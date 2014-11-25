211990d7d3f753f8e15954a862bce490a4d1c2e7
unknown <tomjkidd@gmail.com>
2014-11-25 00:26:45 -0500

    Adding simple Tree data structure with JSON serialization

---
+++ DataStructures/DataStructures/Tree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures {
+    public class Tree<T> {
+        private T data;
+        private Tree<T> parent;
+        private List<Tree<T>> children;
+
+        public T Data {
+            get {
+                return data;
+            }
+        }
+
+        public List<Tree<T>> Children {
+            get {
+                return children;
+            }
+        }
+
+        public Tree(T data, Tree<T> parent = null) {
+            this.data = data;
+            this.parent = parent;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree<T> AddChild(T data) {
+            var node = new Tree<T>(data, this);
+            this.children.Add(node);
+            return node;
+        }
+
+        public static Tree<T> Find(Tree<T> node, Func<Tree<T>, bool> predicate) {
+            Tree<T> foundNode;
+            if(predicate(node)){
+                return node;
+            }
+            foundNode = node.Children.Where(c => predicate(c)).FirstOrDefault();
+            if (foundNode != null) {
+                return foundNode;
+            } else {
+                foreach (var child in node.Children) {
+                    foundNode = Find(child, predicate);
+                    if (foundNode != null) return foundNode;
+                }
+            }
+            return foundNode;
+        }
+    }
+}
---
+++ DataStructures/DataStructures/TreeUsage.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures {
+    public class TreeUsage {
+        public static void TreeUsageMain() {
+            var tree = CreateTree();
+
+            var json = JsonConvert.SerializeObject(tree, Formatting.Indented, new JsonSerializerSettings() {
+                ReferenceLoopHandling = ReferenceLoopHandling.Serialize
+            });
+
+            Console.Write(json);
+            Console.ReadLine();
+        }
+
+        public static Tree<FlatData> CreateTree() {
+            List<FlatData> data = new List<FlatData>() {
+                new FlatData() { ParentId = null, Id = "A", Data = "AData" },
+                new FlatData() { ParentId = "A", Id = "B", Data = "BData" },
+                new FlatData() { ParentId = "A", Id = "C", Data = "CData" },
+                new FlatData() { ParentId = "C", Id = "D", Data = "DData" },
+                new FlatData() { ParentId = "B", Id = "E", Data = "EData" }
+            };
+
+            // Make sure there is only a single root for the tree, this could be relaxed with more effort.
+            Trace.Assert(data.Where(n => n.ParentId == null).Count() == 1);
+
+            Tree<FlatData> root = new Tree<FlatData>(data.Where(n => n.ParentId == null).FirstOrDefault());
+            Tree<FlatData> parent;
+            foreach (FlatData n in data) {
+                if (n.ParentId != null) {
+                    parent = Tree<FlatData>.Find(root, current => current.Data.Id == n.ParentId);
+                    parent.AddChild(n);
+                }
+            }
+
+            return root;
+        }       
+    }
+
+    public class FlatData {
+        public string ParentId { get; set; }
+        public string Id { get; set; }
+        public string Data { get; set; }
+    }
+}
