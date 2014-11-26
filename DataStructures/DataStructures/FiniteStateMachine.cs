7700962d7b00eba252406cdbe297867af69a5d6a
unknown <tomjkidd@gmail.com>
2014-11-26 17:51:21 -0500

    Removing comments

--- DataStructures/DataStructures/FiniteStateMachine.cs
+++ DataStructures/DataStructures/FiniteStateMachine.cs
@@ -6,9 +6,6 @@ using System.Threading.Tasks;
 
 namespace DataStructures {
     public class FiniteStateMachineParameter<S, I, O> {
-        //public List<S> States { get; set; }
-        //public List<I> InputSymbols { get; set; }
-        //public List<O> OutputSymbols { get; set; }
         public S InitialState { get; set; }
         public Func<S, I, S> NextStateFunction { get; set; }
         public Func<S, O> OutputFunction { get; set; }
@@ -22,20 +19,12 @@ namespace DataStructures {
     /// fo: output function, fo(state) -> output
     /// </summary>
     public class FiniteStateMachine <S,I,O> {
-        // What is a good data structure for S?
-        // What is a good interface for iterating through states?
-        //private List<S> states;
-        //private List<I> inputSymbols;
-        //private List<O> outputSymbols;
         private Func<S, I, S> nextStateFunction;
         private Func<S, O> outputFunction;
 
         private S currentState;
 
         public FiniteStateMachine(FiniteStateMachineParameter<S,I,O> p) {
-            //this.states = p.States;
-            //this.inputSymbols = p.InputSymbols;
-            //this.outputSymbols = p.OutputSymbols;
             this.nextStateFunction = p.NextStateFunction;
             this.outputFunction = p.OutputFunction;
             this.currentState = p.InitialState;
